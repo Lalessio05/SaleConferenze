1824424dc4ea83afb4bd134cddd22e960cafec03
Lalessio <catrini.alessio2@gmail.com>
2023-02-13 00:24:13 +0100

    Boh 2

--- SaleConferenze/ConferenceRoom.cs
+++ SaleConferenze/ConferenceRoom.cs
@@ -13,16 +13,26 @@
             }
         }
         public Province Province { get; private set; }
-        public int nTimesUsed { get; private set; }
+        public int NTimesUsed { get; private set; }
         public bool HasProjector { get; private set; }
         public DateTime LastUsed { get; private set; }
+        public ConferenceRoom(int id, string name, int seats, Province province, int nTimesUsed, bool hasProjector, DateTime lastUsed)
+        {
+            Id = id;
+            Name = name;
+            Seats = seats;
+            Province = province;
+            NTimesUsed = nTimesUsed;
+            HasProjector = hasProjector;
+            LastUsed = lastUsed;
+        }
         public override string ToString()
         {
             return $"Room name: {Name}\n" +
                 $"Room ID: {Id}\n" +
                 $"Number of seats: {Seats}\n" +
                 $"Province: {Province} \n" +
-                $"Used {nTimesUsed} times \n" +
+                $"Used {NTimesUsed} times \n" +
                 ((HasProjector)?"Has projector": "Doesn't have projector") + "\n" +
                 $"Last use: {LastUsed.ToShortDateString()}";
 
